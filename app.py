a3729095fb0f89b142680688aec9d51c59767ce5
Sujithra <122441546+nynika@users.noreply.github.com>
2024-05-11 14:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import streamlit as st  # pip install streamlit
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 
-st.set_page_config(page_title="Dashboard", page_icon=":bar_chart:", layout="wide")
-
+st.set_page_config(page_title="Dashboard", page_icon=":bar_chart:", layout="wide",)
 
 # Read Excel
 @st.cache_data
@@ -17,81 +16,123 @@ def get_data_from_excel():
         sheet_name="Sales",
         skiprows=3,
         usecols="B:K",
-        nrows=1000,
+        nrows=558825,
     )
+    df['OrderDate'] = pd.to_datetime(df['OrderDate'], errors='coerce') 
     return df
+df = get_data_from_excel()                                            
 
-df = get_data_from_excel()                                                          # DataFrame
-
-
-# Logo
-st.sidebar.image("./images/relalogo.jpg" , use_column_width=True)
+st.sidebar.image("./images/relalogo.jpg", width=5, use_column_width=True)
 
 
-# Displaying the Twitter logo
-# st.sidebar.image("https://pbs.twimg.com/profile_images/1430718714240933890/-hofz_qF_400x400.jpg", use_column_width=True)
+# Search  
+st.sidebar.header("Search by Department or Doctor Name")
+def search(query):
+    matches = df[df['OrderDepartment'].str.contains(query, case=False) |
+                 df['OrderDoctor'].str.contains(query, case=False)]
+    return matches
 
-#............................................................#
+query = st.sidebar.text_input("Enter department name or doctor name:")
+search_button = st.sidebar.button("Search")
 
-# Sidebar
+if search_button:
+    if query:
+        results = search(query)
+        if not results.empty:
+            results['Doctor_with_Department'] = results['OrderDoctor'] + ' (' + results['OrderDepartment'] + ')'
+            fig_doctors = px.bar(results, x='Doctor_with_Department', y='Net',
+                                 labels={'Net': 'Net Revenue'},
+                                 title='Net Revenue for Doctors by Department',
+                                 color='Net',
+                                 color_continuous_scale='RdBu')
+            fig_doctors.update_layout(xaxis_title='Doctor', yaxis_title='Net Revenue')
+            st.plotly_chart(fig_doctors)
+        else:
+            st.write("No matching data found.")
+    else:
+        st.write("Please enter a search query.")
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            .block-container {padding-top: 0.5rem;} 
+            .css-18e3th9 {padding-top: 0rem;} 
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+# Sidebar filters
 st.sidebar.header("Please Filter Here:")
-order_department = st.sidebar.selectbox(
-    "Select the Department:",
-    options=df["OrderDepartment"].unique(),
-    index=0
-)
+order_department = st.sidebar.selectbox("Select the Department:",options=df["OrderDepartment"].unique(),index=0)
 
-# Filter doctors based on selected department
+# Get doctors based on the selected department
 doctors_options = df[df["OrderDepartment"] == order_department]["OrderDoctor"].unique()
-selected_doctors = st.sidebar.multiselect(
-    "Select the Doctor(s):",
-    options=doctors_options,
-    default=doctors_options
-)
 
+# Here we remove 'default=doctors_options' to avoid pre-selecting all doctors
+selected_doctors = st.sidebar.multiselect("Select the Doctor(s):",options=doctors_options)
 
-# Filter data based on selected department
+# Filter data based on selected department and doctors
 df_selection = df[(df["OrderDepartment"] == order_department) & (df["OrderDoctor"].isin(selected_doctors))]
 
-
-# Check if the dataframe is empty:
+# Display a warning and stop if no data is available
 if df_selection.empty:
     st.warning("No data available based on the current filter settings!")
     st.stop()
 
 
-# Calculate net revenue for each doctor
+# Display KPIs
+st.title(":bar_chart: Revenue Dashboard")
+st.markdown("##")
+total_sales = int(df_selection["Net"].sum())
+st.subheader(f"Total Revenue: INR {total_sales:,}")
+
+# Date range filter and data preparation
+min_date, max_date = st.sidebar.date_input("Select Date Range:", value=[df_selection['OrderDate'].min(), df_selection['OrderDate'].max()])
+filtered_data = df_selection[(df_selection['OrderDate'] >= pd.to_datetime(min_date)) & (df_selection['OrderDate'] <= pd.to_datetime(max_date))]
+
+# Monthly revenue calculation
+revenue_by_month_doctor = filtered_data.groupby([pd.Grouper(key='OrderDate', freq='M'), 'OrderDoctor'])['Net'].sum().unstack(fill_value=0)
+
+# Plotting bar chart with values at the bottom of each bar and larger text size
+fig_revenue_by_month_doctor = go.Figure()
+colors = px.colors.qualitative.Plotly
+for i, doctor in enumerate(revenue_by_month_doctor.columns):
+    fig_revenue_by_month_doctor.add_trace(go.Bar(
+        x=revenue_by_month_doctor.index.strftime('%y-%m'),  # Date format
+        y=revenue_by_month_doctor[doctor],
+        name=doctor,
+        marker_color=colors[i % len(colors)],
+        text=revenue_by_month_doctor[doctor], 
+        textposition='outside',  
+        textfont=dict(family="Arial", size=14, color="black"),  
+    ))
+
+
+fig_revenue_by_month_doctor.update_layout(
+    barmode='stack',
+    title=f"<b>Net Revenue for {order_department} by Month</b>",
+    xaxis=dict(title="Month", tickangle=-45),
+    yaxis=dict(title="Total Net Revenue"),
+    plot_bgcolor='rgba(0,0,0,0)',  # Transparent background
+    paper_bgcolor='rgba(0,0,0,0)',  # Transparent background
+    font=dict(color="black", size=12),
+    margin=dict(l=10, r=10, t=30, b=20),
+)
+
+
+st.plotly_chart(fig_revenue_by_month_doctor, use_container_width=True)
+
+# Calculating net revenue for each doctor
 df_doctors = df_selection.groupby("OrderDoctor")[["Net"]].sum().reset_index()
 
-
-# ---- MAINPAGE ----
-st.title(":bar_chart: Dashboard")
-st.markdown("##")
-
-# TOP KPI's
-total_sales = int(df_selection["Net"].sum())
-average_sale_by_transaction = round(df_selection["Net"].mean(), 2)
-
-left_column, middle_column, right_column = st.columns(3)
-with left_column:
-    st.subheader("Total Revenue:")
-    st.subheader(f"Rup {total_sales:,}")
-
-# with right_column:
-   #  st.subheader("Average Revenue :")
-   # st.subheader(f"Rup {average_sale_by_transaction}")
-
-st.markdown("""---""")                      # Markdown is a lightweight markup language
-                                            # The triple dashes --- in Markdown are often used to represent a horizontal line or divider, 
-                                            #  creating a visual separation between sections of content.
-
 #------------------------------------------------------------------------------------------------------------------------------------#
-    
-
 # SALES BY PRODUCT LINE [BAR CHART]
 sales_by_product_line = df_selection.groupby(by=["ServiceGroup"])[["Net"]].sum().sort_values(by="Net")
 fig_product_sales = px.bar(
-    sales_by_product_line,
+    sales_by_product_line, 
     x="Net",
     y=sales_by_product_line.index,
     orientation="h",
@@ -103,7 +144,6 @@ fig_product_sales.update_layout(
     plot_bgcolor="rgba(0,0,0,0)",
     xaxis=(dict(showgrid=False))
 )
-
 #----------------------------------------------------------------------------------------------------------------------------------#
 
 # SALES BY DOCTOR [HORIZONTAL BAR CHART]
@@ -117,118 +157,31 @@ fig_doctor_sales = px.bar(
     color_discrete_sequence=["#FFA07A"] * len(sales_by_doctor),
     template="plotly_white",
 )
+
 fig_doctor_sales.update_layout(
     plot_bgcolor="rgba(0,0,0,0)",
     yaxis=(dict(showgrid=False))
 )
 
 #----------------------------------------------------------------------------------------------------------------------------------#
-
-# Convert 'OrderDate' to datetime
-df['OrderDate'] = pd.to_datetime(df['OrderDate'])
-
-# Filter data for the selected department and doctors
-filtered_data = df[(df['OrderDepartment'] == order_department) & (df['OrderDoctor'].isin(selected_doctors))]
-
-# Check if the filtered data is empty:
-if filtered_data.empty:
-    st.warning("No data available based on the current filter settings!")
-    st.stop()
-
-# Group by order date and doctor, and calculate total net revenue
-revenue_by_date_doctor = filtered_data.groupby(['OrderDate', 'OrderDoctor'])['Net'].sum().unstack(fill_value=0)
-
-# Plotting line chart for revenue by date for each doctor
-fig_revenue_by_date_doctor = go.Figure()
-
-for doctor in revenue_by_date_doctor.columns:
-    fig_revenue_by_date_doctor.add_trace(go.Scatter(
-        x=revenue_by_date_doctor.index,
-        y=revenue_by_date_doctor[doctor],
-        mode='lines+markers',
-        name=doctor
-    ))
-
-fig_revenue_by_date_doctor.update_layout(
-    title=f"<b>Net Revenue by Order Date for {order_department}</b>",
-    xaxis_title="Order Date",
-    yaxis_title="Total Net Revenue",
-    template="plotly_white"
-)
-
-st.plotly_chart(fig_revenue_by_date_doctor, use_container_width=True)
-
-#-----------------------------------------------------------------------------#
-
-
-# astype(str) , df['A'].astype(float)   #.astype() method in pandas is used to convert the data type of a pandas object to a specified data type. 
-# Timestamp object, %B represents the full month name. 
-# positions (values) where tick marks should appear on an axis.
-
 middle_column, right_column = st.columns(2)
 middle_column.plotly_chart(fig_product_sales, use_container_width=True)
 right_column.plotly_chart(fig_doctor_sales, use_container_width=True)
-
-# -------------- HIDE STREAMLIT STYLE -----------------------------------------------------#
-
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
 #........................................................................................#
 
-# Plotting
+# Plotting net revenue for doctors
 fig_doctors = px.bar(df_doctors, x='OrderDoctor', y='Net',
                      labels={'Net': 'Net Revenue'},
                      title='Net Revenue for Doctors',
                      color='Net',
                      color_continuous_scale='RdBu')
+
 fig_doctors.update_layout(xaxis_title='Doctor', yaxis_title='Net Revenue')
 
-st.plotly_chart(fig_doctors)
+st.plotly_chart(fig_doctors, use_container_width=True)
+
 #........................................................................................................#
 
-# Search function
-def search(query):
-    matches = df[df['OrderDepartment'].str.contains(query, case=False) |
-                  df['OrderDoctor'].str.contains(query, case=False)]
-    return matches
-
-
-# Search input
-query = st.sidebar.text_input("Enter department name or doctor name:")
-
-search_button = st.sidebar.button("Search")
-
-
-# Perform search when button is clicked
-if search_button:
-    if query:
-        results = search(query)
-        if not results.empty:
-            # st.write("Search Results:")
-            # st.write(results)
-
-            # Concatenate doctor names with department names
-            results['Doctor_with_Department'] = results['OrderDoctor'] + ' (' + results['OrderDepartment'] + ')'
-
-            fig_doctors = px.bar(results, x='Doctor_with_Department', y='Net', 
-                                 labels={'Net': 'Net Revenue'},
-                                 title='Net Revenue for Doctors by Department',
-                                 color='Net',
-                                 color_continuous_scale='RdBu')
-            fig_doctors.update_layout(xaxis_title='Doctor', yaxis_title='Net Revenue')
-            st.plotly_chart(fig_doctors)
-                                 
-        else:
-            st.write("No matching data found.")
-    else:
-        st.write("Please enter a search query.")
        
 #....................................................................................................................#
 
